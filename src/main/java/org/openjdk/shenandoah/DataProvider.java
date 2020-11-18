bc55c9c02fd56478fc8b057f6f68b03ffbf41739
Aleksey Shipilev <shade@redhat.com>
2020-11-18 11:18:14 +0100

    Minor cleanups after code inspection

--- src/main/java/org/openjdk/shenandoah/DataProvider.java
+++ src/main/java/org/openjdk/shenandoah/DataProvider.java
@@ -35,13 +35,12 @@ public class DataProvider {
     private final long maxSize;
     private final LongMonitor[] data;
     private final StringMonitor[] matrix;
-    private final LongMonitor timestamp;
     private final LongMonitor status;
 
     public DataProvider(String id) throws Exception {
         MonitoredHost host = MonitoredHost.getMonitoredHost(id);
         MonitoredVm vm = host.getMonitoredVm(new VmIdentifier(id));
-        timestamp = (LongMonitor) vm.findByName("sun.gc.shenandoah.regions.timestamp");
+        LongMonitor timestamp = (LongMonitor) vm.findByName("sun.gc.shenandoah.regions.timestamp");
         LongMonitor max_regions_mon = (LongMonitor) vm.findByName("sun.gc.shenandoah.regions.max_regions");
         maxRegions = (int) max_regions_mon.longValue();
         LongMonitor max_size_mon = (LongMonitor) vm.findByName("sun.gc.shenandoah.regions.region_size");
